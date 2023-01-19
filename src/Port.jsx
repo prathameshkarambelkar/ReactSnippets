0e832d17ee8c4873509e08047fae53b1647f9bef
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-01-20 01:25:52 +0530

    Dhacha is ready

--- src/Port.jsx
+++ src/Port.jsx
@@ -9,17 +9,76 @@ const Port = () => {
     <>
       <Heeeder />
       <section class="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/src/assets/HomeSmall.png')] lg:bg-[url('/src/assets/Home.png')] md:bg-[url('/src/assets/Home.png')] ">
-          <h1 className="text-white text-3xl text-">Home</h1>
+        <h1 className="text-black text-3xl ">Home</h1>
+      </section>
+
+      <div>
+        <header class="flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover bg-[url('/src/assets/AboutMeSmall.png')]  md:bg-[url('/src/assets/AboutMe1.png')]">
+          <img
+            className="rounded-full"
+            alt="Profile Pic"
+            width={200}
+            height={200}
+            src="/src/assets/ProfilePic.jpeg"
+          />
+          <div class="writer inline-block">
+            <div class="writer-text mt-4  flex overflow-hidden border-r-4 text-white ">
+              <h1 className="text-center text-md">Hello 👋🏻, I'm Prathamesh,</h1>
+            </div>
+          </div>
+
+          <p className=" text-white text-2xl text-center font-poppins font-monofont-bold">
+            Android & Front-end Web Developer
+          </p>
+        </header>
+        <section class="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/src/assets/SkillsSmall.png')] lg:bg-[url('/src/assets/Skills.png')] md:bg-[url('/src/assets/Skilla.png')] ">
+          <h1 className="text-black text-3xl text-">Skills</h1>
         </section>
-      {/* <div className="flex bg-gray-900 justify-between">
+
+        <section
+          id="Projects"
+          className="lg:flex-row sm:flex p-2  sm:flex-col sm:items-center sm:justify-center  sm:m-auto  bg-fixed bg-center bg-cover md:bg-[url('/src/assets/Projects.png')]  bg-[url('/src/assets/ProjectsSmall.png')] "
+        >
+          <Projects
+            name={"Demo Project"}
+            technology={["ReactJs", "Tailwind Css", "Vite"]}
+          />
+          <Projects
+            name={"Demo Project"}
+            technology={["ReactJs", "Tailwind Css", "Vite"]}
+          />
+          <Projects
+            name={"Demo Project"}
+            technology={["ReactJs", "Tailwind Css", "Vite"]}
+          />
+          <div className="flex justify  "></div>
+        </section>
+        <section class="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/src/assets/TalksSmall.png')] lg:bg-[url('/src/assets/Talks.png')] md:bg-[url('/src/assets/Talks.png')] ">
+          <h1 className="text-black text-3xl ">Talks</h1>
+        </section>
+        <section class="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/src/assets/ContactMeSmall.png')] lg:bg-[url('/src/assets/ContactMe1.png')] md:bg-[url('/src/assets/ContactMe1.png')] ">
+          <h1 className="text-white text-3xl text-">Lets connect</h1>
+        </section>
+      </div>
+
+      <div></div>
+    </>
+  );
+};
+
+export default Port;
+{
+  /* <div className="flex bg-gray-900 justify-between">
             <h1 className="text-white text-2xl font-mono p-6">Prathamesh Karambelkar</h1>
             <div className="flex">
               <h1 className="p-6 text-white py-6 px-4 text-2xl">About</h1>
               <h1 className="p-6 text-white py-6 px-4 text-2xl">Projects</h1>
               <h1 className="p-6 text-white py-6 px-4 text-2xl">Contact Me!</h1>
             </div>
-          </div> */}
-      {/* <div className="flex justify-between text-white p-8  bg-[#111827] ">
+          </div> */
+}
+{
+  /* <div className="flex justify-between text-white p-8  bg-[#111827] ">
         <div className="ml text-2xl">PK</div>
         <div className="flex">
           <h1 className="px-2">Home</h1>
@@ -27,8 +86,10 @@ const Port = () => {
           <h1 className="px-2">Home</h1>`
           <h1 className="px-2">Home</h1>
         </div>
-      </div> */}
-      {/* <div className="bg-[#111827] flex justify-center ">
+      </div> */
+}
+{
+  /* <div className="bg-[#111827] flex justify-center ">
         <div className="flex flex-col items-center">
           <h1 className="mb-2  p-6  font-mono text-xl text-gray-100 md:text-2xl">
             Hi, <br className="block md:hidden" />
@@ -43,10 +104,10 @@ const Port = () => {
             I create User-friendly and responsive websites
           </h1>
         </div>
-      </div> */}
-      <div>
-        <header class="flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover bg-[url('/src/assets/AboutMeSmall.png')]  md:bg-[url('/src/assets/AboutMe1.png')]">
-          {/* <div className=" flex justify-center ">
+      </div> */
+}
+{
+  /* <div className=" flex justify-center ">
             <div className="flex flex-col items-center">
               <h1 className="mb-2  p-6  font-mono text-xl text-gray-100 md:text-2xl">
                 Hi, <br className="block md:hidden" />
@@ -63,54 +124,5 @@ const Port = () => {
                 <br></br>
               </h1>
             </div>
-          </div> */}
-          <img
-            className="rounded-full"
-            alt="Profile Pic"
-            width={200}
-            height={200}
-            src="/src/assets/ProfilePic.jpeg"
-          />
-          <div class="writer inline-block">
-            <div class="writer-text mt-4  flex overflow-hidden border-r-4 text-white ">
-              <h1 className="text-center text-md">Hello 👋🏻, I'm Prathamesh,</h1>
-
-              {/* <img
-                width={50}
-                height={50}
-                src="https://raw.githubusercontent.com/TheDudeThatCode/TheDudeThatCode/master/Assets/Hi.gif"
-              /> */}
-            </div>
-          </div>
-
-          <p className=" text-white text-2xl text-center font-poppins font-monofont-bold">
-            Android & Front-end Web Developer
-          </p>
-        </header>
-
-        <section id="Projects" className="lg:flex-row sm:flex  sm:flex-col sm:items-center sm:justify-center  sm:m-auto  bg-fixed bg-center bg-cover md:bg-[url('/src/assets/Projects.png')]  bg-[url('/src/assets/ProjectsSmall.png')] ">
-          <Projects
-            name={"Demo Project"}
-            technology={["ReactJs", "Tailwind Css", "Vite"]}
-          />
-          <Projects
-            name={"Demo Project"}
-            technology={["ReactJs", "Tailwind Css", "Vite"]}
-          />
-          <Projects
-            name={"Demo Project"}
-            technology={["ReactJs", "Tailwind Css", "Vite"]}
-          />
-          <div className="flex justify  "></div>
-        </section>
-        <section class="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/src/assets/ContactMeSmall.png')] lg:bg-[url('/src/assets/ContactMe1.png')] md:bg-[url('/src/assets/ContactMe1.png')] ">
-          <h1 className="text-white text-3xl text-">Lets connect</h1>
-        </section>
-      </div>
-
-      <div></div>
-    </>
-  );
-};
-
-export default Port;
+          </div> */
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = (props) => {
   const listItems = tech.map((stack) => <li>{stack}</li>);
 
   return (
-    <div className="border-2 border-black  rounded-md p-2 m-6  ">
+    <div className="border-2 border-white text-white  rounded-md p-2 m-6  ">
       <img
         width={300}
         height={300}
@@ -19,7 +19,7 @@ const Projects = (props) => {
         src="https://i.pinimg.com/474x/09/65/b9/0965b93f250920c1ee8a9a2b0ba0c291.jpg"
       />
       <h1 className="underline text-center">{props.name}</h1>
-      <hr className="border-black m-2" />
+      <hr className="border-white m-2" />
       <h2 className="ml-2">Tech-Stack Used:</h2>
       <ul className="ml-5 list-disc">{listItems}</ul>
     </div>
